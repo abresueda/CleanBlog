93e40799fabd36bbaf1ba818b7a147e89c532d6b
abresueda <abre.sueda@gmail.com>
2024-12-10 18:46:36 +0300

    clean blog 2

--- index.js
+++ index.js
@@ -1,15 +1,37 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
+const path = require('path');
+// EJS şablon motorunu ayarlama
+app.set('view engine', 'ejs');
+
+// Views klasörünü belirt
+app.set('views', path.join(__dirname, 'views'));
+
+//Statik dosyaları sunmak için middleware
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 // Basit bir GET isteği
-app.get('/', (req, res) => {
+/*app.get('/', (req, res) => {
   const blog = {
     id: 1,
     title: 'Blog title',
     description: 'Blog description',
   };
   res.send(blog);
+});*/
+
+app.get('/', (req, res) => {
+    res.render('index', { title: 'Clean Blog' });
+});
+
+app.get('/about', (req,res) => {
+    res.render('about', { title: 'About Us' });
+});
+
+app.get('add', (req, res) => {
+    res.render('add', { title: 'Add New Post' });
 });
 
 // Sunucuyu dinleme
